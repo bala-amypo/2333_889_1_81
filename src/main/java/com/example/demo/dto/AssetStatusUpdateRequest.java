00de7631dec20d659966bd509d29282d47bd7090
bala-amypo <bala@amypo.in>
2025-12-21 05:06:39 +0000

    User commit

--- src/main/java/com/example/demo/dto/AssetStatusUpdateRequest.java
+++ src/main/java/com/example/demo/dto/AssetStatusUpdateRequest.java
@@ -1,59 +1,20 @@
 package com.example.demo.dto;
 
 public class AssetStatusUpdateRequest {
-
-    private String assetTag;
-    private String assetType;
-    private String model;
-    private String purchaseDate;
+    
     private String status;
-    private Long currentHolderId;
-
-    public String getAssetTag() {
-        return assetTag;
+    
+    public AssetStatusUpdateRequest() {}
+    
+    public AssetStatusUpdateRequest(String status) {
+        this.status = status;
     }
-
-    public void setAssetTag(String assetTag) {
-        this.assetTag = assetTag;
-    }
-
-    public String getAssetType() {
-        return assetType;
-    }
-
-    public void setAssetType(String assetType) {
-        this.assetType = assetType;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getPurchaseDate() {
-        return purchaseDate;
-    }
-
-    public void setPurchaseDate(String purchaseDate) {
-        this.purchaseDate = purchaseDate;
-    }
-
+    
     public String getStatus() {
         return status;
     }
-
+    
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public Long getCurrentHolderId() {
-        return currentHolderId;
-    }
-
-    public void setCurrentHolderId(Long currentHolderId) {
-        this.currentHolderId = currentHolderId;
     }
 }
