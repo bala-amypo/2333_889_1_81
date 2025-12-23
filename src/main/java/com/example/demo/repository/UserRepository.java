3d5b50d2b568d133238947c0ae1444eac6dca17f
bala-amypo <bala@amypo.in>
2025-12-23 05:35:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,8 +1,36 @@
-package com.example.demo.repository;
+package com.example.demo.controller;
 
 import com.example.demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // CREATE
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    // READ ALL
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    // READ BY ID
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
 }
