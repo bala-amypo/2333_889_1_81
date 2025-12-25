0229890091443ee280c56e3558ce96fe8e4a429f
bala-amypo <bala@amypo.in>
2025-12-25 05:11:27 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -12,9 +14,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9309.pro604cr.amypo.ai/")
-                ));
-        }
+                .info(new Info().title("Digital Asset Lifecycle API").version("1.0"))
+                .servers(List.of(new Server().url("http://localhost:8080")));
+    }
 }
